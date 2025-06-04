6eda2ae2d8fca13b1291784be7ba09d3bdd0eea0
HilaryGH <hilarygebremedhn28@gmail.com>
2025-06-04 09:05:31 +0300

    Fix Vite base path for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/Hamlin-capital/',
+  base: '/',
   plugins: [react(), tailwindcss(),svgr() ],
 })
